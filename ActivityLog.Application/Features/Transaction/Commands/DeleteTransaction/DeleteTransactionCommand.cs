7c095b79115438cf9a90ade75275ae6113da5f40
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-04-08 17:42:17 -0500

    CQRS Delete Transaction
    Se realiza la implementacion de el comando Delete de la entidad Transaction

---
+++ ActivityLog.Application/Features/Transaction/Commands/DeleteTransaction/DeleteTransactionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLog.Application.Features.Transaction.Commands.DeleteTransaction
+{
+	public class DeleteTransactionCommand : IRequest<string>
+	{
+		public int Id { get; set; }
+	}
+}
---
+++ ActivityLog.Application/Features/Transaction/Commands/DeleteTransaction/DeleteTransactionCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using ActivityLog.Application.Contracts.Persistence;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityLog.Application.Features.Transaction.Commands.DeleteTransaction
+{
+	public class DeleteTransactionCommandHandler : IRequestHandler<DeleteTransactionCommand, string>
+	{
+		private readonly ITransactionRepository _transactionRepository;
+
+		public DeleteTransactionCommandHandler(ITransactionRepository transactionRepository)
+		{
+			_transactionRepository = transactionRepository;
+		}
+
+		public async Task<string> Handle(DeleteTransactionCommand request, CancellationToken cancellationToken)
+		{
+			var transactionToDelete = await _transactionRepository.GetByIdAsync(request.Id);
+			if (transactionToDelete == null)
+			{
+				throw new ArgumentNullException(nameof(transactionToDelete));
+			}
+			transactionToDelete.IsDeleted = true;
+			await _transactionRepository.UpdateAsync(transactionToDelete);
+
+			return $"Transaction {request.Id} Deleted";
+
+
+		}
+	}
+}
