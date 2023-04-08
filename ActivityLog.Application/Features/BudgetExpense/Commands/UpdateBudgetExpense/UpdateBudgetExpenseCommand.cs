f6f244f4933d96be67c1892b8d9a65adb1c9e442
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-04-08 11:52:59 -0500

    CQRS Update BudgetExpense
    Se realiza la implementacion de el comando Update de la entidad BudgetExpense

--- ActivityLog.Application/Features/BudgetExpense/Commands/UpdateBudgetExpense/UpdateBudgetExpenseCommand.cs
+++ ActivityLog.Application/Features/BudgetExpense/Commands/UpdateBudgetExpense/UpdateBudgetExpenseCommand.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ActivityLog.Domain.Entities;
+using MediatR;
 
 namespace ActivityLog.Application.Features.BudgetExpense.Commands.UpdateBudgetExpense
 {
-	public class UpdateBudgetExpenseCommand
+	public class UpdateBudgetExpenseCommand : IRequest<string>
 	{
+		public int Id { get; set; }
+
+		public decimal Amount { get; set; }
+
+		public string? Description { get; set; }
+
+		public int CategoryId { get; set; }
+
 	}
 }
--- ActivityLog.Application/Features/BudgetExpense/Commands/UpdateBudgetExpense/UpdateBudgetExpenseCommandHandler.cs
+++ ActivityLog.Application/Features/BudgetExpense/Commands/UpdateBudgetExpense/UpdateBudgetExpenseCommandHandler.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ActivityLog.Application.Contracts.Persistence;
+using AutoMapper;
+using MediatR;
 
 namespace ActivityLog.Application.Features.BudgetExpense.Commands.UpdateBudgetExpense
 {
-	public class UpdateBudgetExpenseCommandHandler
+	public class UpdateBudgetExpenseCommandHandler : IRequestHandler<UpdateBudgetExpenseCommand, string>
 	{
+		private readonly IBudgetExpenseRepository _BudgetExpenseRepository;
+		private readonly IMapper _mapper;
+
+		public UpdateBudgetExpenseCommandHandler(IBudgetExpenseRepository budgetExpenseRepository, IMapper mapper)
+		{
+			_BudgetExpenseRepository = budgetExpenseRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<string> Handle(UpdateBudgetExpenseCommand request, CancellationToken cancellationToken)
+		{
+			var budgetExpenseToUpdate = await _BudgetExpenseRepository.GetByIdAsync(request.Id);
+			if (budgetExpenseToUpdate == null)
+			{
+				throw new ArgumentNullException(nameof(budgetExpenseToUpdate));
+			}
+
+			_mapper.Map(request, budgetExpenseToUpdate, typeof(UpdateBudgetExpenseCommand), typeof(Domain.Entities.BudgetExpense));
+
+			await _BudgetExpenseRepository.UpdateAsync(budgetExpenseToUpdate);
+
+			return $"Budget Expense {request.Id} Updated";
+		}
 	}
 }
