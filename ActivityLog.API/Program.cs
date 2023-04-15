b002c6e56f80400932b7479755f6b73e4a923c4f
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-04-15 02:07:13 -0500

    Configuracion CORS ActivityLog

--- ActivityLog.API/Program.cs
+++ ActivityLog.API/Program.cs
@@ -1,7 +1,20 @@
 using ActivityLog.Application;
 using ActivityLog.Infrastructure;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("http://localhost:4200")
+							.SetIsOriginAllowedToAllowWildcardSubdomains()
+							.AllowAnyHeader()
+							.AllowAnyMethod();
+					  });
+});
 
 // Add services to the container.
 
@@ -23,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
--- Application.ActivityLog.Test/BudgetRepositoryTest.cs
+++ Application.ActivityLog.Test/BudgetRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using ActivityLog.Application.Contracts.Persistence;
+using ActivityLog.Domain.Entities;
 using Moq;
 
 namespace Application.ActivityLog.Test
@@ -12,6 +13,40 @@ namespace Application.ActivityLog.Test
 			_mockBudgetRepository = new Mock<IBudgetRepository>();
 		}
 
+		[Fact]
+		public async Task CreateTBudgetAsync()
+		{
+			//Arrange
+			var budgetToCreate = new Budget
+			{
+				IdUser = "6434318b1e01e1b13701d864",
+				Name = "Prueba1",
+				TargetMonth = DateTime.Now,
+				Balance = 5000,
+				MonthlyTotal = 2600,
+				State = "Pending",
+			};
 
+			var budget = new Budget
+			{
+				IdUser = "6434318b1e01e1b13701d864",
+				Name = "Prueba1",
+				TargetMonth = DateTime.Now,
+				Balance = 5000,
+				MonthlyTotal = 2600,
+				State = "Pending",
+			};
+
+			_mockBudgetRepository.Setup(x => x.CreateAsync(budgetToCreate)).ReturnsAsync(budget);
+
+			//Act
+
+			var budgetResult = await _mockBudgetRepository.Object.CreateAsync(budgetToCreate);
+
+			//Assert
+
+			Assert.Equal(budget, budgetResult);
+
+		}
 	}
 }
